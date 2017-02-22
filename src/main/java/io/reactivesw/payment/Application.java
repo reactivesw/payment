eb55abe5523616d75eb998accf8ebe8a4457fba0
umasuo <liuquan89@gmail.com>
2017-02-22 19:50:26 +0800

    add k8s support

--- src/main/java/io/reactivesw/payment/Application.java
+++ src/main/java/io/reactivesw/payment/Application.java
@@ -1,6 +1,7 @@
 package io.reactivesw.payment;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -8,7 +9,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * Created by umasuo on 17/2/9.
  */
 @EnableSwagger2
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "io.reactivesw")
+@EnableAutoConfiguration
 public class Application {
 
   public static void main(String[] args) {
