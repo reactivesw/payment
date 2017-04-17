fbc3f28ddd8960c3402eeddd79027f465ac0741a
Davis <davis.dai@go6d.com>
2017-04-17 12:06:02 +0800

    edit deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -47,7 +47,7 @@ echo '=============================finish gcloud container cluster credentials==
 
 #kubectl create deployments and service by .yaml file
 echo '=============================kubectl create yaml==================================='
-kubectl delete deployment payment --ignore-not-found=true
-kubectl delete service payment --ignore-not-found=true
+kubectl delete deployment payment --namespace=dev --ignore-not-found=true
+kubectl delete service payment --namespace=dev --ignore-not-found=true
 kubectl create -f ./deploy/k8s.yaml
 echo '=============================finish kubectl create yaml============================'
