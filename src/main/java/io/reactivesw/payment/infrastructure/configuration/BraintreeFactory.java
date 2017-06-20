7403028f614273133423098e609983906b8e9d89
davis999 <davis.dai@go6d.com>
2017-06-20 14:44:47 +0800

    fix config bug

--- src/main/java/io/reactivesw/payment/infrastructure/configuration/BraintreeFactory.java
+++ src/main/java/io/reactivesw/payment/infrastructure/configuration/BraintreeFactory.java
@@ -43,7 +43,7 @@ public class BraintreeFactory {
           BraintreeEnvironmentUtils.getEnvironment(config.getEnvironment()),
           config.getMerchantId(),
           config.getPublicKey(),
-          config.getPublicKey());
+          config.getPrivateKey());
     }
     return gateway;
   }
